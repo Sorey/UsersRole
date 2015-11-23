c89403c3605bf7196749e4312d0e5183ad3190b8
Sorey <3sorey4@gmail.com>
2015-11-23 16:12:30 +0200

    start master 23.11.2015

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   validates :first_name, :last_name, length: { minimum: 2}
   validates :password,  length: { minimum: 6}, confirmation: true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i #, on: :create
   #validates :password_confirmation, presence: true
 
   #validates :password, length: { minimum: 6, message: 'must be have minimum 8 characters.' }
